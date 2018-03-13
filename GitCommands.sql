dd9df6233129dfe8d13f6c0325971e0de8a0c1d9
julianvasquez1992 <julianvasquez1992@gmail.com>
2018-03-12 20:47:24 -0500

    Update GitCommands.sql

--- GitCommands.sql
+++ GitCommands.sql
@@ -8,6 +8,9 @@ git branch
 git branch -d [name_of_your_new_branch]
 git branch -D [name_of_your_new_branch]
 
+git remote add origin https://github.com/julianvasquez1992/OracleCommands.git
+git push -u origin master
+
 //To do merge
 git checkout master
 git merge Namebranch
